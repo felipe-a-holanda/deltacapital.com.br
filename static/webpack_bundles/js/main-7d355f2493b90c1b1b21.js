e22c2d245eb85dc258f56bff906568cbf05336f4
Felipe Holanda <flp9001@gmail.com>
2020-05-30 15:54:33 -0300

    Removed compress CSS from template

---
+++ static/webpack_bundles/js/main-7d355f2493b90c1b1b21.js
@@ -0,0 +1,112 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./static/js/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./static/js/main.js":
+/*!***************************!*\
+  !*** ./static/js/main.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/main.scss */ \"./static/sass/main.scss\");\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sass_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n/*\r\n\tIndustrious by TEMPLATED\r\n\ttemplated.co @templatedco\r\n\tReleased for free under the Creative Commons Attribution 3.0 license (templated.co/license)\r\n*/\n\n\n(function ($) {\n  var $window = $(window),\n      $banner = $('#banner'),\n      $body = $('body'); // Breakpoints.\n\n  breakpoints({\n    \"default\": ['1681px', null],\n    xlarge: ['1281px', '1680px'],\n    large: ['981px', '1280px'],\n    medium: ['737px', '980px'],\n    small: ['481px', '736px'],\n    xsmall: ['361px', '480px'],\n    xxsmall: [null, '360px']\n  }); // Play initial animations on page load.\n\n  $window.on('load', function () {\n    window.setTimeout(function () {\n      $body.removeClass('is-preload');\n    }, 100);\n  }); // Menu.\n\n  $('#menu').append('<a href=\"#menu\" class=\"close\"></a>').appendTo($body).panel({\n    target: $body,\n    visibleClass: 'is-menu-visible',\n    delay: 500,\n    hideOnClick: true,\n    hideOnSwipe: true,\n    resetScroll: true,\n    resetForms: true,\n    side: 'right'\n  });\n})(jQuery);\n\n//# sourceURL=webpack:///./static/js/main.js?");
+
+/***/ }),
+
+/***/ "./static/sass/main.scss":
+/*!*******************************!*\
+  !*** ./static/sass/main.scss ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleNotFoundError: Module not found: Error: Can't resolve '../css/font-awesome.min.css' in '/home/darpan/Dropbox/projects/deltacapital.com.br/static/sass'\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/webpack/lib/Compilation.js:925:10\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/webpack/lib/NormalModuleFactory.js:401:22\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/webpack/lib/NormalModuleFactory.js:130:21\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/webpack/lib/NormalModuleFactory.js:224:22\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/neo-async/async.js:2830:7\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/neo-async/async.js:6877:13\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/webpack/lib/NormalModuleFactory.js:214:25\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:213:14\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:13:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/UnsafeCachePlugin.js:44:7\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:13:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:25:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:67:43\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:14:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:25:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:67:43\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:14:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/Resolver.js:285:5\\n    at eval (eval at create (/home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:13:1)\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/DirectoryExistsPlugin.js:27:15\\n    at /home/darpan/Dropbox/projects/deltacapital.com.br/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)\");\n\n//# sourceURL=webpack:///./static/sass/main.scss?");
+
+/***/ })
+
+/******/ });
