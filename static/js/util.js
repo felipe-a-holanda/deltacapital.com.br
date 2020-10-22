7393361facb6821e42dd96c8be69f629a55caa49
Felipe Holanda <flp9001@gmail.com>
2020-10-22 14:47:34 -0300

    Fix mobile topbar

--- static/js/util.js
+++ static/js/util.js
@@ -601,7 +601,7 @@ function show_menu() {
 
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {sticky_navbar(); console.log("sticky");};
+window.onscroll = function() {sticky_navbar();};
 
 // Get the navbar
 var navbar = document.getElementById("myTopnav");
@@ -611,7 +611,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function sticky_navbar() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
