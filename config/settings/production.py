75de76be39fa2c95b8ffe58251e62eb309ac1b13
Felipe Holanda <flp9001@gmail.com>
2021-02-01 10:09:26 -0300

    Use whitenoise on prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -78,7 +78,8 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "apps.utils.storages.StaticRootS3Boto3Storage"
+#STATICFILES_STORAGE = "apps.utils.storages.StaticRootS3Boto3Storage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
 # MEDIA
