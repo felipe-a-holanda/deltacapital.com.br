e03aa16927c6dfda7c5c34aef5b45fa71ae41887
robson <robson.oliveira@oowlish.com>
2021-03-10 10:57:44 -0300

    Fix django error update redme

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,7 +7,7 @@ from .base import ROOT_DIR
 DEBUG = env("DEBUG")
 
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-INSTALLED_APPS += ["django_extensions", "debug_toolbar"]  # noqa F405
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 
